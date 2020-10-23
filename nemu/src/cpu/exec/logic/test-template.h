2d4275ebd05ab41242649c8303ddb8d9e275ea60
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-23 23:21:02 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:21:02 up  7:47,  1 user,  load average: 0.22, 0.21, 0.23 e7c84f98380862543fb8ad791287fc9c8de489fb

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,7 +3,23 @@
 #define instr test
 
 static void do_execute() {
-    // if(op_src->val & op_dest->val)
+    uint32_t res = 0;
+    res += op_src->val & op_dest->val;
+    cpu.CF = 0;
+    cpu.OF = 0;
+    if(res == 0) {
+        cpu.ZF = 1;
+    }
+    else {
+        cpu.ZF = 0;
+    }
+    cpu.SF = (res & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1);
+    uint32_t tmp = res & 1;
+    int i;
+    for(i = 1; i < 8; i++) {
+        tmp = tmp ^ (res & (1 << i) >> i);
+    }
+    cpu.PF = !tmp;
 }
 
 make_instr_helper(r2rm)
