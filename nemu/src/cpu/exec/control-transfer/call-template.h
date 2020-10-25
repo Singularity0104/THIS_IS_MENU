3bd0bd87967ec8b54e9f2d87c59ac448f6efb598
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-25 19:47:28 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:47:28 up 2 days,  4:13,  1 user,  load average: 0.55, 0.70, 0.80 c177846e21c5775044410a68f86c236dc6425cfa

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -5,12 +5,12 @@
 static void do_execute() {
     cpu.esp -= DATA_BYTE;
     if(DATA_BYTE == 2) {
-        uint16_t ip = (uint16_t)(cpu.eip & 0x0000ffff);
+        uint16_t ip = (uint16_t)((cpu.eip + concat(decode_i_, SUFFIX)(cpu.eip)) & 0x0000ffff);
         MEM_W(cpu.esp, ip);
         cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
     }
     else if(DATA_BYTE == 4) {
-        MEM_W(cpu.esp, cpu.eip);
+        MEM_W(cpu.esp, (cpu.eip + concat(decode_i_, SUFFIX)(cpu.eip)));
         cpu.eip = cpu.eip + op_src->val;
     }
 	print_asm_template1();
