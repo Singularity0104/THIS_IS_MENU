e4f64186de8f88ebea3e6d0fae080cdfc2d616d0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-25 19:29:51 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:29:51 up 2 days,  3:56,  1 user,  load average: 0.79, 0.91, 0.85 5f3010ab7b7797b0258617be16f3fe11f959262

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -3,7 +3,15 @@
 #define instr ret
 
 make_helper(concat(ret_, SUFFIX)) {
-	
+    if(DATA_BYTE == 2) {
+        cpu.eip =  MEM_R(cpu.esp);
+        cpu.eip = cpu.eip & 0x0000ffff;
+    }
+    else if(DATA_BYTE == 4) {
+        cpu.eip =  MEM_R(cpu.esp);
+    }
+	cpu.esp += DATA_BYTE;
+	print_asm_template1();
 	return 1;
 }
 
