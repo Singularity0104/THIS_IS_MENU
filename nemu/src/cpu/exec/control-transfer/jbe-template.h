582d390909660d2670150274cf2f45137287b352
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 18:37:58 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:37:58 up 4 days,  3:04,  1 user,  load average: 1.93, 1.64, 1.57 17f4114a7be4184316bba3e53be5836833dd8621

--- nemu/src/cpu/exec/control-transfer/jbe-template.h
+++ nemu/src/cpu/exec/control-transfer/jbe-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	if(cpu.ZF == 1 || cpu.CF == 1) {
         if((op_src->val & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1) == 1) {
             cpu.eip = cpu.eip + ((~op_src->val) + 1);
-            printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+            printf("%x aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n", cpu.eip);
         }
         else {
             cpu.eip = cpu.eip + op_src->val;
