2f13e7a5cfd551420de13dacae672e2172e44213
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 17:52:50 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:52:50 up 4 days,  2:19,  1 user,  load average: 1.69, 1.66, 1.44 90d661c2c7b70cbdc370f23971717ede429f42a3

--- nemu/src/cpu/exec/binary-arithmetic/cmp-template.h
+++ nemu/src/cpu/exec/binary-arithmetic/cmp-template.h
@@ -5,8 +5,18 @@
 static void do_execute() {
 	uint32_t res = 0;
     res = res + (op_dest->val - op_src->val);
-    cpu.CF = 0;
-    cpu.OF = 0;
+    if((uint32_t)op_dest->val < (uint32_t)op_src->val) {
+        cpu.CF = 1;
+    }
+    else {
+        cpu.CF = 0;
+    }
+    if(((op_dest->val >> (DATA_BYTE * 8 - 1)) ^ (op_src->val >> (8 * DATA_BYTE - 1))) & (!((res >> (DATA_BYTE * 8 - 1)) ^ (op_src->val >> (8 * DATA_BYTE - 1)))) & 1) {
+        cpu.OF = 1;
+    }
+    else {
+        cpu.OF = 0;
+    }
     if(res == 0) {
         cpu.ZF = 1;
     }
