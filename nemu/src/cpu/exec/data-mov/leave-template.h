ee24c8243351b9ed4fb4b2e23cd26827c610c5b0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 20:57:53 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:57:53 up 4 days,  5:24,  1 user,  load average: 0.86, 1.29, 1.32 35c3ea8bd2fc9b63dbf7c1d1f8283e9ba8074f70

---
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr leave
+
+make_helper(concat(leave_, SUFFIX)) {
+    cpu.esp = cpu.ebp;
+    cpu.ebp =  MEM_R(cpu.esp);
+    cpu.esp += DATA_BYTE;
+    print_asm_template1();
+    printf("%x    aaaaaaaaaaaaaaaaa\n", cpu.eip);
+    return 0;
+}
+
+#include "cpu/exec/template-end.h"
