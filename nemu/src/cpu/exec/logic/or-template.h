de62701e4aa423f0a37dc3b3d42abf1a6c250f1d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 21:18:33 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:18:33 up 5 days,  5:44,  1 user,  load average: 0.79, 0.49, 0.40 a3d280cfe5484a991d050f4b7f03c8f8b667633d

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -3,11 +3,26 @@
 #define instr or
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val | op_src->val;
-	OPERAND_W(op_dest, result);
+	DATA_TYPE res = op_dest->val | op_src->val;
+	OPERAND_W(op_dest, res);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	// panic("please implement me");
+	cpu.CF = 0;
+	cpu.OF = 0;
+	if(res == 0) {
+        cpu.ZF = 1;
+    }
+    else {
+        cpu.ZF = 0;
+    }
+    cpu.SF = (res & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1);
+    uint32_t tmp = res & 1;
+    int i;
+    for(i = 1; i < 8; i++) {
+        tmp = tmp ^ (res & (1 << i) >> i);
+    }
+    cpu.PF = !tmp;
 
 	print_asm_template2();
 }
