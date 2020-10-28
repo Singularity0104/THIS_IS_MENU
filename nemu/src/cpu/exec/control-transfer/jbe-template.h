f76a7936783d474df3c82d7c1fabd3da08587895
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:28:46 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:28:46 up 5 days,  4:54,  1 user,  load average: 0.42, 0.56, 0.49 b8cc416d744e3ce2ae036c324183f1ffd51a876

--- nemu/src/cpu/exec/control-transfer/jbe-template.h
+++ nemu/src/cpu/exec/control-transfer/jbe-template.h
@@ -4,12 +4,14 @@
 
 static void do_execute() {
 	if(cpu.ZF == 1 || cpu.CF == 1) {
-        uint32_t s = 0;
-        if((op_src->val & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1) == 1) {
-            s = ((1u << (DATA_BYTE * 8 - 1)) - 1) + (1u << (DATA_BYTE * 8 - 1));
-            s = ~s;
-        }
-        cpu.eip = cpu.eip + op_src->val + s;
+        // uint32_t s = 0;
+        // if((op_src->val & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1) == 1) {
+        //     s = ((1u << (DATA_BYTE * 8 - 1)) - 1) + (1u << (DATA_BYTE * 8 - 1));
+        //     s = ~s;
+        // }
+        // cpu.eip = cpu.eip + op_src->val + s;
+        DATA_TYPE_S offset = op_src->val;
+        cpu.eip = cpu.eip + offset;
         if(DATA_BYTE == 2) {
             cpu.eip = cpu.eip & 0x0000ffff;
         }
