8793ee87a30c63c47fe4932a70a32851d4356132
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 13:19:54 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:19:54 up  2:13,  1 user,  load average: 0.70, 0.73, 0.73 332020b98ffbdb85f167ce90ac1899138cc53a

--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -14,14 +14,14 @@ static void do_execute() {
 make_instr_helper(i)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(jmp_rm_, SUFFIX)) {
-	uint32_t len = concat(decode_rm_, SUFFIX)(eip + 1);
+	concat(decode_rm_, SUFFIX)(eip + 1);
 	swaddr_t addr = op_src->val;
 	cpu.eip = addr;
 	if(DATA_BYTE == 2) {
 		cpu.eip = cpu.eip & 0x0000ffff;
 	}
 	print_asm_template1();
-	return len + 1;
+	return 0;
 }
 #endif
 
