b82cbcbb7ac5366c02a3abfbbf3f6e00eccc5668
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 20:49:24 +0800

    >  run 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:49:24 up 1 day,  9:42,  1 user,  load average: 0.58, 0.66, 0.61 c76e93149c6106dbebdf9ec5429593555cd10970

--- testcase/src/my-hello-str.c
+++ testcase/src/my-hello-str.c
@@ -5,6 +5,15 @@
 char buf[128];
 
 int main() {
-	sprintf(buf, "%s", "Hello world!\n");
+	buf[0] = 'a';
+	buf[1] = 'a';
+	buf[2] = 'a';
+	buf[3] = 'a';
+	buf[4] = 'a';
+	buf[5] = 'a';
+	buf[6] = 'a';
+	buf[7] = 'a';
+	buf[8] = 0;
+	nemu_assert(strcmp(buf, "aaaaaaaa") == 0);
 	return 0;
 }
