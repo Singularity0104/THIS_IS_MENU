0b9f17b7a66a9f0629bd6b1e41386ab17eda446d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 22:38:26 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:38:26 up 1 day, 11:31,  1 user,  load average: 1.03, 0.90, 0.81 df5eab3e99160ab8358caaca413077b724cde355

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -15,4 +15,19 @@ make_helper(concat(ret_, SUFFIX)) {
 	return 0;
 }
 
+make_helper(concat(ret_i_, SUFFIX)) {
+    uint32_t len = decode_i_w(eip + 1);
+    if(DATA_BYTE == 2) {
+        cpu.eip =  MEM_R(cpu.esp);
+        cpu.eip = cpu.eip & 0x0000ffff;
+    }
+    else if(DATA_BYTE == 4) {
+        cpu.eip =  MEM_R(cpu.esp);
+    }
+    cpu.eip = cpu.eip + op_src->val;
+	cpu.esp = cpu.esp + DATA_BYTE;
+	print_asm_template1();
+	return len;
+}
+
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control-transfer/ret.c
+++ nemu/src/cpu/exec/control-transfer/ret.c
@@ -10,3 +10,4 @@
 
 
 make_helper_v(ret)
+make_helper_v(ret_i)
