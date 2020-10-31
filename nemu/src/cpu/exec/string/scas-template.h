2a78eb20a8a5ea64f9ff77c85efdbb8a09d31b33
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 16:31:04 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:31:04 up 1 day,  5:24,  1 user,  load average: 1.00, 1.01, 0.96 4ccbd84fb2d167a4e7ed1aa1ee381272cfd2e8f6

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -3,36 +3,37 @@
 #define instr scas
 
 make_helper(concat(scas_, SUFFIX)) {
-    DATA_TYPE_S des = REG(R_EAX);
-    DATA_TYPE_S src = MEM_R(cpu.edi);
-    DATA_TYPE_S res = des - src;
-    if((uint32_t)des < (uint32_t)src) {
-        cpu.CF = 1;
-    }
-    else {
-        cpu.CF = 0;
-    }
-    if(((des >> (DATA_BYTE * 8 - 1)) ^ (src >> (8 * DATA_BYTE - 1))) & (~((res >> (DATA_BYTE * 8 - 1)) ^ (src >> (8 * DATA_BYTE - 1)))) & 1) {
-        cpu.OF = 1;
-    }
-    else {
-        cpu.OF = 0;
-    }
-    if(res == 0) {
-        cpu.ZF = 1;
-    }
-    else {
-        cpu.ZF = 0;
-    }
-    cpu.SF = (res & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1);
-    uint32_t tmp = res & 1;
-    int i;
-    for(i = 1; i < 8; i++) {
-        tmp = tmp ^ (res & (1 << i) >> i);
-    }
-    cpu.PF = !tmp;
-    print_asm(str(instr) str(SUFFIX));
-    return 0;
+    // DATA_TYPE_S des = REG(R_EAX);
+    // DATA_TYPE_S src = MEM_R(cpu.edi);
+    // DATA_TYPE_S res = des - src;
+    // if((uint32_t)des < (uint32_t)src) {
+    //     cpu.CF = 1;
+    // }
+    // else {
+    //     cpu.CF = 0;
+    // }
+    // if(((des >> (DATA_BYTE * 8 - 1)) ^ (src >> (8 * DATA_BYTE - 1))) & (~((res >> (DATA_BYTE * 8 - 1)) ^ (src >> (8 * DATA_BYTE - 1)))) & 1) {
+    //     cpu.OF = 1;
+    // }
+    // else {
+    //     cpu.OF = 0;
+    // }
+    // if(res == 0) {
+    //     cpu.ZF = 1;
+    // }
+    // else {
+    //     cpu.ZF = 0;
+    // }
+    // cpu.SF = (res & (1 << (DATA_BYTE * 8 - 1))) >> (DATA_BYTE * 8 - 1);
+    // uint32_t tmp = res & 1;
+    // int i;
+    // for(i = 1; i < 8; i++) {
+    //     tmp = tmp ^ (res & (1 << i) >> i);
+    // }
+    // cpu.PF = !tmp;
+    // print_asm(str(instr) str(SUFFIX));
+    printf("00000000000000000000000000000000000000000\n");
+    return 1;
 }
 
 #include "cpu/exec/template-end.h"
