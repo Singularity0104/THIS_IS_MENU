452322605eb246d15f868bf5d09a9095399834ad
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 20:45:42 +0800

    >  run 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:45:42 up 1 day,  9:38,  1 user,  load average: 0.59, 0.60, 0.57 96e5c5501a0969bf15985647717f5434470d75e8

--- testcase/src/my-hello-str.c
+++ testcase/src/my-hello-str.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include "trap.h"
 
-char buf[] = "fuck_nemu";
+char buf[128];
 
 int main() {
-	nemu_assert(strcmp(buf, "fuck_nemu") == 0);
+	sprintf(buf, "%s", "Hello world!\n");
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	return 0;
 }
