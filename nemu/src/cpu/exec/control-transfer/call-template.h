e9009e865bd733c06197ff6e7404dc3441d6b7d0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 16:21:53 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:21:53 up 1 day, 15:48,  1 user,  load average: 1.23, 1.33, 1.39 cf6b42e0f1dc14e387b26cb531f35276f5376cc8

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -3,20 +3,35 @@
 #define instr call
 
 static void do_execute() {
-    cpu.esp = cpu.esp - DATA_BYTE;
-    if(DATA_BYTE == 2) {
-        uint16_t ip = (uint16_t)((cpu.eip + 1 + DATA_BYTE) & 0x0000ffff);
-        MEM_W(cpu.esp, ip);
-        cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+    if(op_src->type == OP_TYPE_IMM){
+        cpu.esp = cpu.esp - DATA_BYTE;
+        if(DATA_BYTE == 2) {
+            uint16_t ip = (uint16_t)((cpu.eip + 1 + DATA_BYTE) & 0x0000ffff);
+            MEM_W(cpu.esp, ip);
+            cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+        }
+        else if(DATA_BYTE == 4) {
+            MEM_W(cpu.esp, (cpu.eip + 1 + DATA_BYTE));
+            cpu.eip = cpu.eip + op_src->val;
+        }
     }
-    else if(DATA_BYTE == 4) {
-        MEM_W(cpu.esp, (cpu.eip + 1 + DATA_BYTE));
-        cpu.eip = cpu.eip + op_src->val;
+    else if(op_src->type == OP_TYPE_REG) {
+        cpu.esp = cpu.esp - DATA_BYTE;
+        if(DATA_BYTE == 2) {
+            uint16_t ip = (uint16_t)((cpu.eip + 1 + DATA_BYTE) & 0x0000ffff);
+            MEM_W(cpu.esp, ip);
+            cpu.eip = (cpu.eip + MEM_R(op_src->val)) & 0x0000ffff;
+        }
+        else if(DATA_BYTE == 4) {
+            MEM_W(cpu.esp, (cpu.eip + 1 + DATA_BYTE));
+            cpu.eip = cpu.eip + MEM_R(op_src->val);
+        }
     }
     
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
