760e8b800bea00f30dd9965e61dea5e20bd9ce31
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 17:04:38 +0800

    >  run 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:04:38 up 1 day, 16:30,  1 user,  load average: 2.44, 1.72, 1.37 83f53bc46680419f2ec510f09ebfa30b2f71c848

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,5 +1,4 @@
 #include "../FLOAT.h"
-#include "string.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
 	nemu_assert(0);
@@ -29,46 +28,14 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 */
 
 	// nemu_assert(0);
-	FLOAT tmp_a = *((int *)&a);
-	FLOAT tmp_b = *((int *)&b);
+	FLOAT tmp_a = a;
+	FLOAT tmp_b = b;
 	FLOAT a_h = tmp_a & 0xffff0000;
 	FLOAT a_l = tmp_a & 0xffff;
 	FLOAT res = ((a_h / tmp_b) << 16) + ((a_l << 16) / tmp_b);
 	return res;
 }
 
-// FLOAT f2F(float a) {
-// 	/* You should figure out how to convert `a' into FLOAT without
-// 	 * introducing x87 floating point instructions. Else you can
-// 	 * not run this code in NEMU before implementing x87 floating
-// 	 * point instructions, which is contrary to our expectation.
-// 	 *
-// 	 * Hint: The bit representation of `a' is already on the
-// 	 * stack. How do you retrieve it to another variable without
-// 	 * performing arithmetic operations on it directly?
-// 	 */
-// 	// nemu_assert(0);
-// 	FLOAT res = 0;
-// 	FLOAT tmp = 0;
-// 	memcpy(&tmp, &a, 4);
-// 	int s = (tmp >> 31) & 1;
-// 	int e = (tmp >> 23) & 0xff;
-// 	int m = tmp & 0x7fffff;
-// 	e = e - 127;
-// 	m = m + (1 << 23);
-// 	int offset = e - 7;
-// 	if(offset > 0) {
-// 		res = m << offset;
-// 	}
-// 	else {
-// 		offset = -offset;
-// 		res = m >> offset;
-// 	}
-// 	if(s == 1) {
-// 		res = ~res + 1;
-// 	}
-// 	return res;
-// }
 FLOAT f2F(float a) {
 	/* You should figure out how to convert `a' into FLOAT without
 	 * introducing x87 floating point instructions. Else you can
@@ -79,31 +46,29 @@ FLOAT f2F(float a) {
 	 * stack. How do you retrieve it to another variable without
 	 * performing arithmetic operations on it directly?
 	 */
-	//   提示：“ a”的位表示形式已经在堆栈中。 如何在不直接对其执行算术运算的情况下将其检索到另一个变量？
-
 	// nemu_assert(0);
-	// float : 1 sign + 8 exp + 23 frac
-	// 值：	规格化：					(-1)^s * 2^(exp - 127) * (1.frac) = (-1)^s * 2^(exp - 150) * (1frac)
-	// 	   非规格化：	exp == 0		(-1)^s * 2^(1-127) * 0.frac = (-1)^s * 2^(1-150) * frac
-	// 读取float a
-	int bb[1];
-	memcpy((void *)bb, (void *)&a, 4);
-	int b = bb[0];
-	int sign = b >> 31;
-	int exp = (b >> 23) & 0xff;
-	FLOAT frac = b & 0x7fffff;
-	// 规格化和非规格化，全部指数化
-	if (exp != 0) frac += 1 << 23;		//如果是规格化的，小数点前有1
-	else exp = 1;						//如果非规格话，那么exp当1处理
-	exp -= 150;
-	// 得到FLOAT
-	// frac << exp << 16		即		frac << (exp + 16)
-	if(exp + 16 > 0) frac <<= exp + 16;
-	else if(exp + 16 < 0) frac >>= -(exp + 16);
-	// 返回，加上符号
-	if(!sign) return frac;
-	else return -frac;
+	FLOAT res = 0;
+	FLOAT *ptr = (int *)&a;
+	res = *ptr;
+	// int s = (tmp >> 31) & 1;
+	// int e = (tmp >> 23) & 0xff;
+	// int m = tmp & 0x7fffff;
+	// e = e - 127;
+	// m = m + (1 << 23);
+	// int offset = e - 7;
+	// if(offset > 0) {
+	// 	res = m << offset;
+	// }
+	// else {
+	// 	offset = -offset;
+	// 	res = m >> offset;
+	// }
+	// if(s == 1) {
+	// 	res = ~res + 1;
+	// }
+	return res;
 }
+
 
 
 FLOAT Fabs(FLOAT a) {
