dbd5b712123abb74575cd67b61468e0a4af7bceb
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 22:31:19 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:31:19 up  5:01,  1 user,  load average: 0.82, 0.74, 0.69 d65a1670195a895131bc18f3861ec5f776ec6a36

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -19,15 +19,15 @@ static void do_execute() {
 make_instr_helper(i)
 
 make_helper(concat(call_rm_, SUFFIX)) {
-    concat(decode_rm_, SUFFIX)(eip + 1);
+    int len = concat(decode_rm_, SUFFIX)(eip + 1);
     cpu.esp = cpu.esp - DATA_BYTE;
     if(DATA_BYTE == 2) {
-        uint16_t ip = (uint16_t)((cpu.eip + 1 + DATA_BYTE) & 0x0000ffff);
+        uint16_t ip = (uint16_t)((cpu.eip + 1 + len) & 0x0000ffff);
         MEM_W(cpu.esp, ip);
         cpu.eip = op_src->val & 0x0000ffff;
     }
     else if(DATA_BYTE == 4) {
-        MEM_W(cpu.esp, (cpu.eip + 1 + DATA_BYTE));
+        MEM_W(cpu.esp, (cpu.eip + 1 + len));
         cpu.eip = op_src->val;
     }
 	print_asm_template1();
