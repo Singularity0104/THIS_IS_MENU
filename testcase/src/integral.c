c48d595c61269730a7829ed7e67d79d15ecae3c4
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 22:03:47 +0800

    >  run 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:03:47 up  4:34,  1 user,  load average: 0.56, 0.85, 0.81 d0fd5d7dee92770077b681f152871de020c83050

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -39,15 +39,16 @@ FLOAT f(FLOAT x) {
 }
 
 FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
-	int k;
-	FLOAT s,h;
-	h = F_div_int((b - a), n);
-	s = F_div_int(fun(a) + fun(b), 2 );
-	for(k = 1; k < n; k ++) {
-		s += fun(a + F_mul_int(h, k));
-	}
-	s = F_mul_F(s, h);
-	return s;
+	// int k;
+	// FLOAT s,h;
+	// h = F_div_int((b - a), n);
+	// s = F_div_int(fun(a) + fun(b), 2 );
+	// for(k = 1; k < n; k ++) {
+	// 	s += fun(a + F_mul_int(h, k));
+	// }
+	// s = F_mul_F(s, h);
+	// return s;
+	return 0;
 }
 
 int main() { 
