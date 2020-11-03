fb15623fbdc1c9c9f62810d5a61be996fd2ee109
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 22:06:21 +0800

    >  run 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:06:21 up  4:36,  1 user,  load average: 0.78, 0.81, 0.80 56859226bc5fa5e6a5df7ce43f0368e43bf551a5

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -43,7 +43,6 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	FLOAT s,h;
 	h = F_div_int((b - a), n);
 	s = F_div_int(fun(a) + fun(b), 2 );
-	nemu_assert(0);
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
@@ -53,6 +52,7 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 
 int main() { 
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	nemu_assert(0);
 	FLOAT ans = f2F(0.551222);
 
 	nemu_assert(Fabs(a - ans) < f2F(1e-4));
