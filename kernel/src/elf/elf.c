22bf2bd06516ce67d16cea12833afe2944b5bb0e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 12:34:36 +0800

    >  compile kernel 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:34:36 up 2 days, 19:05,  1 user,  load average: 0.81, 0.60, 0.41 a6f8aaaacf581ae43743d76a396e80516de16938

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -44,12 +44,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
-			 
+			 ramdisk_read((uint8_t *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
+			memset((void *)(ELF_OFFSET_IN_DISK + ph->p_offset + ph->p_filesz), 0, (ph->p_memsz - ph->p_filesz));
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
