5f0c9325de35c69f0ef6837a755878a2e2c0f263
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 16:39:34 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:39:34 up 5 days, 23:10,  1 user,  load average: 0.57, 0.64, 0.49 e4dd09b9153267475bd979b1ee3f304fff703363

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -167,21 +167,21 @@ static int cmd_b(char *args) {
 	Elf32_Sym *symtab = GETsymtab();
 	int nr_symtab_entry = GETnr_symtab_entry();
 	uint32_t offset = 0;
+	uint32_t cur_arg = 0;
 	int i;
 	for(i = 0; i < nr_symtab_entry; i++) {
 		if((symtab[i].st_info & 0xf) == STT_FUNC) {
 			if(cur_eip >= symtab[i].st_value && cur_eip <= symtab[i].st_value + symtab[i].st_size) {
 				offset = symtab[i].st_name;
 				printf("%02d 0x%08x~0x%08x %s\n", cnt, symtab[i].st_value, symtab[i].st_value + symtab[i].st_size, strtab + symtab[i].st_name);
-				uint32_t arg;
-				arg = swaddr_read(cur_ebp + 8, 4);
-				printf("	arg_1 0x%08x %d\n", arg, arg);
-				arg = swaddr_read(cur_ebp + 12, 4);
-				printf("	arg_2 0x%08x %d\n", arg, arg);
-				arg = swaddr_read(cur_ebp + 16, 4);
-				printf("	arg_3 0x%08x %d\n", arg, arg);
-				arg = swaddr_read(cur_ebp + 20, 4);
-				printf("	arg_4 0x%08x %d\n", arg, arg);
+				cur_arg = swaddr_read(cur_ebp + 8, 4);
+				printf("	arg_1 0x%08x %d\n", cur_arg, cur_arg);
+				cur_arg = swaddr_read(cur_ebp + 12, 4);
+				printf("	arg_2 0x%08x %d\n", cur_arg, cur_arg);
+				cur_arg = swaddr_read(cur_ebp + 16, 4);
+				printf("	arg_3 0x%08x %d\n", cur_arg, cur_arg);
+				cur_arg = swaddr_read(cur_ebp + 20, 4);
+				printf("	arg_4 0x%08x %d\n", cur_arg, cur_arg);
 			}
 		}
 	}
@@ -194,15 +194,14 @@ static int cmd_b(char *args) {
 				if(cur_ret >= symtab[i].st_value && cur_ret <= symtab[i].st_value + symtab[i].st_size) {
 					offset = symtab[i].st_name;
 					printf("%02d 0x%08x~0x%08x %s\n", cnt, symtab[i].st_value, symtab[i].st_value + symtab[i].st_size, strtab + symtab[i].st_name);
-					uint32_t arg;
-					arg = swaddr_read(cur_ebp + 8, 4);
-					printf("	arg_1 0x%08x %d\n", arg, arg);
-					arg = swaddr_read(cur_ebp + 12, 4);
-					printf("	arg_2 0x%08x %d\n", arg, arg);
-					arg = swaddr_read(cur_ebp + 16, 4);
-					printf("	arg_3 0x%08x %d\n", arg, arg);
-					arg = swaddr_read(cur_ebp + 20, 4);
-					printf("	arg_4 0x%08x %d\n", arg, arg);
+					cur_arg = swaddr_read(cur_ebp + 8, 4);
+					printf("	arg_1 0x%08x %d\n", cur_arg, cur_arg);
+					cur_arg = swaddr_read(cur_ebp + 12, 4);
+					printf("	arg_2 0x%08x %d\n", cur_arg, cur_arg);
+					cur_arg = swaddr_read(cur_ebp + 16, 4);
+					printf("	arg_3 0x%08x %d\n", cur_arg, cur_arg);
+					cur_arg = swaddr_read(cur_ebp + 20, 4);
+					printf("	arg_4 0x%08x %d\n", cur_arg, cur_arg);
 				}
 			}
 		}
