4315c5ac654f1c30f4f596a849aa451985021d82
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 16:29:59 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:29:59 up 5 days, 23:00,  1 user,  load average: 0.29, 0.37, 0.36 2f75557f141631d95e489eb77351ec07aa91c228

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -173,6 +173,15 @@ static int cmd_b(char *args) {
 			if(cur_eip >= symtab[i].st_value && cur_eip <= symtab[i].st_value + symtab[i].st_size) {
 				offset = symtab[i].st_name;
 				printf("%02d 0x%08x~0x%08x %s\n", cnt, symtab[i].st_value, symtab[i].st_value + symtab[i].st_size, strtab + symtab[i].st_name);
+				uint32_t arg;
+				arg = swaddr_read(cur_ebp + 8, 4);
+				printf("	arg_1 0x%08x %d", arg, arg);
+				arg = swaddr_read(cur_ebp + 12, 4);
+				printf("	arg_1 0x%08x %d", arg, arg);
+				arg = swaddr_read(cur_ebp + 16, 4);
+				printf("	arg_1 0x%08x %d", arg, arg);
+				arg = swaddr_read(cur_ebp + 20, 4);
+				printf("	arg_1 0x%08x %d", arg, arg);
 			}
 		}
 	}
@@ -185,6 +194,15 @@ static int cmd_b(char *args) {
 				if(cur_ret >= symtab[i].st_value && cur_ret <= symtab[i].st_value + symtab[i].st_size) {
 					offset = symtab[i].st_name;
 					printf("%02d 0x%08x~0x%08x %s\n", cnt, symtab[i].st_value, symtab[i].st_value + symtab[i].st_size, strtab + symtab[i].st_name);
+					uint32_t arg;
+					arg = swaddr_read(cur_ebp + 8, 4);
+					printf("	arg_1 0x%08x %d", arg, arg);
+					arg = swaddr_read(cur_ebp + 12, 4);
+					printf("	arg_1 0x%08x %d", arg, arg);
+					arg = swaddr_read(cur_ebp + 16, 4);
+					printf("	arg_1 0x%08x %d", arg, arg);
+					arg = swaddr_read(cur_ebp + 20, 4);
+					printf("	arg_1 0x%08x %d", arg, arg);
 				}
 			}
 		}
