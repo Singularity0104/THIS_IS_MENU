087a03af008062dfd4ebd96772965b55f05dfcae
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-09 23:27:40 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:27:40 up 1 day, 12:31,  1 user,  load average: 0.40, 0.79, 0.57 a2487f4424d50ca83717b2d4b9c5d7457ac740a1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -5,7 +5,7 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
-#define MYDEBUG 1
+#define MYDEBUG 0
 #define DEBUGIN 0
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 #if MYDEBUG
