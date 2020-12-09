0e3266773cc69d66f5aceefbf5b856386dcaed1f
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-09 19:08:19 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:08:19 up 1 day,  8:12,  1 user,  load average: 0.36, 0.36, 0.36 51d0f2d042f382844a31bf6d1b3dc637679883cc

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -5,36 +5,40 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
-
+#define MYDEBUG 0
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	// if(cache_1_find(&Cache_1, addr) == NULL) {
-	// 	char *new_ptr = cache_1_replace(&Cache_1, addr);
-	// 	hwaddr_t begin_addr = addr & (~((uint32_t)0xffffffff >> (32 - Cache_1_B_bit)));
-	// 	int i;
-	// 	for(i = 0; i < Cache_1_B_size; i++, begin_addr++) {
-	// 		char tmp_data = dram_read(begin_addr, 1) & 0xff;
-	// 		new_ptr[i] = tmp_data;
-	// 	}
-	// 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	// }
-	// uint32_t offset = addr & ((uint32_t)0xffffffff >> (32 - Cache_1_B_bit));
-	// char *ptr = cache_1_find(&Cache_1, addr);
-	// char tmp = 0;
-	// uint32_t data = 0;
-	// int i;
-	// for(i = 0; i < len; i++, offset++) {
-	// 	if(offset >= Cache_1_B_size) {
-	// 		ptr = cache_1_find(&Cache_1, addr + i);
-	// 		offset = 0;
-	// 	}
-	// 	tmp = *ptr;
-	// 	ptr++;
-	// 	data = data << 8;
-	// 	data += ((uint32_t)tmp & 0xff);
-	// }
-	// return data;
+#if MYDEBUG
+	if(cache_1_find(&Cache_1, addr) == NULL) {
+		char *new_ptr = cache_1_replace(&Cache_1, addr);
+		hwaddr_t begin_addr = addr & (~((uint32_t)0xffffffff >> (32 - Cache_1_B_bit)));
+		int i;
+		for(i = 0; i < Cache_1_B_size; i++, begin_addr++) {
+			char tmp_data = dram_read(begin_addr, 1) & 0xff;
+			new_ptr[i] = tmp_data;
+		}
+		return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	}
+	uint32_t offset = addr & ((uint32_t)0xffffffff >> (32 - Cache_1_B_bit));
+	char *ptr = cache_1_find(&Cache_1, addr);
+	char tmp = 0;
+	uint32_t data = 0;
+	int i;
+	for(i = 0; i < len; i++, offset++) {
+		if(offset >= Cache_1_B_size) {
+			ptr = cache_1_find(&Cache_1, addr + i);
+			offset = 0;
+		}
+		tmp = *ptr;
+		ptr++;
+		data = data << 8;
+		data += ((uint32_t)tmp & 0xff);
+	}
+	return data;
+#else
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+#endif
 }
+#undef MYDEBUG
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	dram_write(addr, len, data);
