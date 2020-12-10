26153cb7a105c3ff51287e5c8144bd3498a53dbf
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-10 21:05:18 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:05:18 up 2 days, 10:09,  1 user,  load average: 2.17, 1.65, 1.41 4e2ea9ad390bfabd6ef77650fa334094e377e5bb

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -117,7 +117,7 @@ void cpu_exec(volatile uint32_t n) {
 		// 	/* Output some dots while executing the program. */
 		// 	fputc('.', stderr);
 		// }
-		if((n & 0xffff) == 0) {
+		if((n & 0xfff) == 0) {
 			/* Output some dots while executing the program. */
 			fputc(poem[poem_index % poem_length], stderr);
 			poem_index++;
