5ca9b4b676476661e4acc975c435c48fad17337d
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-09 17:02:53 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:02:53 up 6 days, 21:35,  1 user,  load average: 1.10, 1.57, 1.58 a7803cc4c3a86e3c8d5a14bbf1ead6bf47b974f8

--- nemu/src/cpu/exec/data-mov/movcr-template.h
+++ nemu/src/cpu/exec/data-mov/movcr-template.h
@@ -4,17 +4,17 @@
 
 make_helper(movcr_r2rm_l) {
     int len = decode_r2rm_l(eip);
-    if(op_dest->addr == 0) {
-        cpu.cr0.val = op_src->val;
-    }
+    // if(op_dest->addr == 0) {
+    //     cpu.cr0.val = op_src->val;
+    // }
 	print_asm_template2();
     return len;
 }
 make_helper(movcr_rm2r_l) {
     int len = decode_rm2r_l(eip);
-    if(op_src->addr == 0) {
-        REG(op_dest->reg) = cpu.cr0.val;
-    }
+    // if(op_src->addr == 0) {
+    //     REG(op_dest->reg) = cpu.cr0.val;
+    // }
 	print_asm_template2();
     return len;
 }
