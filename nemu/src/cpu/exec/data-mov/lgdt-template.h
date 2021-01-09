1774ccad56ee5f6bce6b09b82a64812493acd370
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-09 16:56:24 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:56:24 up 6 days, 21:28,  1 user,  load average: 2.15, 1.76, 1.56 309ad19b140b40fbe9cd1f2b211f7c82ef634455

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -3,15 +3,15 @@
 #define instr lgdt
 
 static void do_execute() {
-	swaddr_t gdt_addr_ptr = op_src->addr;
-    cpu.gdtr.limit = lnaddr_read(gdt_addr_ptr, 2);
-    gdt_addr_ptr += 2;
-#if DATA_BYTE == 2
-    cpu.gdtr.base = lnaddr_read(gdt_addr_ptr, 3);
-#endif
-#if DATA_BYTE == 4
-    cpu.gdtr.base = lnaddr_read(gdt_addr_ptr, 4);
-#endif
+// 	swaddr_t gdt_addr_ptr = op_src->addr;
+//     cpu.gdtr.limit = lnaddr_read(gdt_addr_ptr, 2);
+//     gdt_addr_ptr += 2;
+// #if DATA_BYTE == 2
+//     cpu.gdtr.base = lnaddr_read(gdt_addr_ptr, 3);
+// #endif
+// #if DATA_BYTE == 4
+//     cpu.gdtr.base = lnaddr_read(gdt_addr_ptr, 4);
+// #endif
 }
 
 make_instr_helper(rm)
