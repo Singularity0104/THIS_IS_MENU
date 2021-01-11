c5035d13b3c33c49e1c79a84fef756a96b3ee5ad
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-12 01:01:16 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:01:16 up 12:43,  1 user,  load average: 0.67, 0.51, 0.54 651fd64c2b4b6d1f4a2f0a79be78051b99c9d464

--- nemu/src/cpu/exec/data-mov/movcr-template.h
+++ nemu/src/cpu/exec/data-mov/movcr-template.h
@@ -3,22 +3,22 @@
 #define instr movcr
 
 make_helper(movcr_rm2r_l) {
-    int len = decode_rm2r_l(eip + 1);
-    if(op_dest->reg == 0) {
-        cpu.cr0.val = op_src->val;
+    // int len = decode_rm2r_l(eip + 1);
+    // if(op_dest->reg == 0) {
+    //     cpu.cr0.val = op_src->val;
 
-    }
-	print_asm_template2();
-    len = 3;
-    return len;
+    // }
+	// print_asm_template2();
+    return 2;
 }
 make_helper(movcr_r2rm_l) {
-    int len = decode_r2rm_l(eip + 1);
-    if(op_src->reg == 0) {
-        REG(op_dest->reg) = cpu.cr0.val;
-    }
-	print_asm_template2();
-    return len;
+    // int len = decode_r2rm_l(eip + 1);
+    // if(op_src->reg == 0) {
+    //     REG(op_dest->reg) = cpu.cr0.val;
+    // }
+	// print_asm_template2();
+    // return len;
+    return 2;
 }
 
 #include "cpu/exec/template-end.h"
