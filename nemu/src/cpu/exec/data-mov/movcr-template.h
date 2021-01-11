f77774d50df8c0af1948eb9ecaf0b2f154dd93db
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-12 01:24:47 +0800

    >  compile NEMU 3019244289 singularity Linux DELL-Inspiron 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:24:47 up 13:06,  1 user,  load average: 1.21, 0.61, 0.50 27ea0919524de1b75e4dc700ff0295af3ac13019

--- nemu/src/cpu/exec/data-mov/movcr-template.h
+++ nemu/src/cpu/exec/data-mov/movcr-template.h
@@ -2,16 +2,25 @@
 
 #define instr movcr
 
-make_helper(movcr_rm2cr_l) {
+make_helper(movcr_r2cr_l) {
     // int len = decode_rm2r_l(eip + 1);
     // if(op_dest->reg == 0) {
     //     cpu.cr0.val = op_src->val;
 
     // }
 	// print_asm_template2();
+    // return 2;
+    uint8_t modrm = instr_fetch(eip + 1, 1);
+    uint8_t cr = ((modrm >> 3) & 0x7);
+    uint8_t reg = modrm & 0x7;
+    uint32_t src = REG(reg);
+    if(cr == 0) {
+        cpu.cr0.val = src;
+    }
+    print_asm(str(instr) str(SUFFIX) " %s%d,%s", "cr", cr, REG_NAME(reg));
     return 2;
 }
-make_helper(movcr_cr2rm_l) {
+make_helper(movcr_cr2r_l) {
     // int len = decode_r2rm_l(eip + 1);
     // if(op_src->reg == 0) {
     //     REG(op_dest->reg) = cpu.cr0.val;
